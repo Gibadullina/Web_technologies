7683a0f477ef034df51736f70bd9f1e5cd3b1353
Gibadullina <gibadullina.elina@inbox.ru>
2022-02-15 23:05:42 +0500

    Done ex1

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <p>Лабораторная работа № 1<p> 
     <ul>
  <li><a href="http://gibadullina/lab1.html">Задание 1</a></li>
+ <li><a href="http://gibadullina/lab2.html">Задание 2</a></li>
+ <li><a href="http://gibadullina/lab3.html">Задание 3</a></li>
+ <li><a href="http://gibadullina/lab4.html">Задание 4</a></li>
+ <li><a href="http://gibadullina/lab5.html">Задание 5</a></li>
+ <li><a href="http://gibadullina/lab6.html">Задание 6</a></li>
+ <li><a href="http://gibadullina/lab7.html">Задание 7</a></li>
+
