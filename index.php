97ae03412a8de0e3ccefe8c33b5d27dc8f0834e2
Gibadullina <gibadullina.elina@inbox.ru>
2022-02-21 00:15:33 +0500

    Change script of example ex4

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
  <li><a href="http://gibadullina/lab1.html">Задание 1</a></li>
  <li><a href="http://gibadullina/lab2.html">Задание 2</a></li>
  <li><a href="http://gibadullina/lab3.html">Задание 3</a></li>
+ <li><a href="http://gibadullina/lab4example.html">Задание 4 (пример)</a></li>
  <li><a href="http://gibadullina/lab4.html">Задание 4</a></li>
  <li><a href="http://gibadullina/lab5.html">Задание 5</a></li>
  <li><a href="http://gibadullina/lab6.html">Задание 6</a></li>
