830aeea58c2c7eff982c1207008248b2a5b6b018
Gibadullina <gibadullina.elina@inbox.ru>
2022-02-24 21:29:03 +0500

    Корректировка

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <p>Лабораторная работа № 1<p> 
     <ul>
- <li><a href="http://gibadullina/lab1.html">Задание 1</a></li>
- <li><a href="http://gibadullina/lab2.html">Задание 2</a></li>
- <li><a href="http://gibadullina/lab3.html">Задание 3</a></li>
- <li><a href="http://gibadullina/lab4example.html">Задание 4 (пример)</a></li>
- <li><a href="http://gibadullina/lab4.html">Задание 4</a></li>
- <li><a href="http://gibadullina/lab5.html">Задание 5</a></li>
- <li><a href="http://gibadullina/lab6.html">Задание 6</a></li>
- <li><a href="http://gibadullina/lab7.html">Задание 7</a></li>
+ <li><a href="lab1.html">Задание 1</a></li>
+ <li><a href="lab2.html">Задание 2</a></li>
+ <li><a href="lab3.html">Задание 3</a></li>
+ <li><a href="lab4example.html">Задание 4 (пример)</a></li>
+ <li><a href="lab4.html">Задание 4</a></li>
+ <li><a href="lab5.html">Задание 5</a></li>
+ <li><a href="lab6.html">Задание 6</a></li>
+ <li><a href="lab7example.html">Задание 7 (пример)</a></li>
+ <li><a href="lab7.html">Задание 7</a></li>
 
