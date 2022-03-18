dce53815462656b8692a06070883645c656e164d
Gibadullina <gibadullina.elina@inbox.ru>
2022-03-18 22:53:28 +0500

    Ex 3

--- index.php
+++ index.php
@@ -15,12 +15,10 @@
 	 <li><a href="lab-2/test.html">Тест</a></li>
  <li><a href="lab-2/lab2.html">Пример</a></li>	 
  <li><a href="lab-2/lab_2-1.html">Задание 1</a></li>
- <li><a href="lab2.html">Задание 2</a></li>
- <li><a href="lab3.html">Задание 3</a></li>
- <li><a href="lab4example.html">Задание 4 (пример)</a></li>
- <li><a href="lab4.html">Задание 4</a></li>
- <li><a href="lab5.html">Задание 5</a></li>
- <li><a href="lab6.html">Задание 6</a></li>
- <li><a href="lab7example.html">Задание 7 (пример)</a></li>
- <li><a href="lab7.html">Задание 7</a></li>
+ <li><a href="lab-2/lab_2-2.html">Задание 2</a></li>
+ <li><a href="lab-2/lab_2-3.html">Задание 3</a></li>
+ <li><a href="lab-2/lab_2-4.html">Задание 4</a></li>
+ <li><a href="lab-2/lab_2-5.html">Задание 5</a></li>
+ <li><a href="lab-2/lab_2-6.html">Задание 6</a></li>
+ <li><a href="lab-2/lab_2-7.html">Задание 7</a></li>
  </ul>
