ce816cd23a3c64a777c8c7a50db1f1a2fced63c8
Gibadullina <gibadullina.elina@inbox.ru>
2022-03-18 17:58:49 +0500

    Ex 1 - lab 2

--- index.php
+++ index.php
@@ -9,4 +9,17 @@
  <li><a href="lab6.html">Задание 6</a></li>
  <li><a href="lab7example.html">Задание 7 (пример)</a></li>
  <li><a href="lab7.html">Задание 7</a></li>
-
+  </ul>
+Лабораторная работа № 2 
+    <ul>
+	 <li><a href="lab-2/test.html">Тест</a></li>
+ <li><a href="lab-2/lab_2-1.html">Задание 1</a></li>
+ <li><a href="lab2.html">Задание 2</a></li>
+ <li><a href="lab3.html">Задание 3</a></li>
+ <li><a href="lab4example.html">Задание 4 (пример)</a></li>
+ <li><a href="lab4.html">Задание 4</a></li>
+ <li><a href="lab5.html">Задание 5</a></li>
+ <li><a href="lab6.html">Задание 6</a></li>
+ <li><a href="lab7example.html">Задание 7 (пример)</a></li>
+ <li><a href="lab7.html">Задание 7</a></li>
+ </ul>
