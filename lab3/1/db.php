36e6a5d2a068c1e2ee1c1afbb60f463e75204a96
Gibadullina <gibadullina.elina@inbox.ru>
2022-05-04 23:59:06 +0500

    l 3

--- lab3/1/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("HOST", "localhost");
-define("USER", "root");
-define("PASSWORD", "");
-define("DB_NAME", "news");
-$db_connect = mysqli_connect(HOST, USER, PASSWORD, DB_NAME); 
-//mysql_selectdb(DB_NAME,$db_connect);
-//mysqli_set_charset('utf8');
-?>
